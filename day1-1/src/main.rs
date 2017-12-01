32202062e6d15ba7f21c4f491000ea13b75188cf
Mikael Zayenz Lagerkvist <zayenz@gmail.com>
2017-12-01 10:36:02 +0100

    Solve Day 1, Puzzle 1

--- day1-1/src/main.rs
+++ day1-1/src/main.rs
@@ -1,3 +1,33 @@
+#[macro_use]
+extern crate failure;
+use failure::Error;
+
+use std::{io, process};
+
+fn run() -> Result<(), Error>
+{
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+
+    let nums: Vec<u32> = input.chars().flat_map(|c| c.to_digit(10)).collect();
+    let pairs = nums.iter().zip(nums.iter().cycle().skip(1));
+    let equal_pairs = pairs.filter(|&(a, b)| a == b);
+
+    let result: u32 = equal_pairs.map(|(a,_b)| a).sum();
+
+    println!("{}", result);
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    match  run() {
+        Ok(()) => process::exit(0),
+        Err(error) => {
+            for cause in error.causes() {
+                eprintln!("{}", cause)
+            }
+            process::exit(1)
+        }
+    }
 }
