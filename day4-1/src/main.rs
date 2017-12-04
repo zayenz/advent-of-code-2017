9f049ab355212d533ea480ae4d32c9a521d0ce33
Mikael Zayenz Lagerkvist <zayenz@gmail.com>
2017-12-04 10:20:03 +0100

    Solve Day 4-1

---
+++ day4-1/src/main.rs
@@ -0,0 +1,58 @@
+#[macro_use]
+extern crate failure;
+use failure::Error;
+
+use std::{io, process};
+use std::collections::HashSet;
+use std::iter::FromIterator;
+use std::io::BufRead;
+
+
+fn read_input() -> Result<Vec<Vec<String>>, Error> {
+    let mut input: Vec<Vec<String>> = Vec::new();
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let row: Vec<String> = line?.split_whitespace().map(|word| word.into()).collect();
+        if !row.is_empty() {
+            input.push(row);
+        }
+    }
+    Ok(input)
+}
+
+fn checksum_row(row: &Vec<String>) -> Result<u32, Error> {
+    let words: HashSet<&String> = HashSet::from_iter(row.iter());
+    if words.len() == row.len() {
+        Ok(1)
+    } else {
+        Ok(0)
+    }
+
+}
+
+fn run() -> Result<(), Error> {
+    let input = read_input()?;
+    let row_checksums = input
+        .iter()
+        .map(|row| checksum_row(row))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let checksum: u32 = row_checksums.iter().sum();
+
+    println!("{}", checksum);
+
+    Ok(())
+}
+
+
+fn main() {
+    match run() {
+        Ok(()) => process::exit(0),
+        Err(error) => {
+            for cause in error.causes() {
+                eprintln!("{}", cause)
+            }
+            process::exit(1)
+        }
+    }
+}
