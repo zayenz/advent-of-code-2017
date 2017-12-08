b60995e9c1222005141fe237bced78338de122b9
Mikael Zayenz Lagerkvist <zayenz@gmail.com>
2017-12-08 08:02:11 +0100

    Solve Day 8 1&2

---
+++ day8-1/src/main.rs
@@ -0,0 +1,158 @@
+#[macro_use]
+extern crate failure;
+use failure::Error;
+
+use std::{io, process};
+use std::collections::HashMap;
+use std::io::BufRead;
+use std::str::FromStr;
+
+
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+enum Comparison {
+    Less,
+    LessEqual,
+    Equal,
+    GreaterEqual,
+    Greater,
+    NotEqual,
+}
+
+use Comparison::*;
+
+impl Comparison {
+    fn compare(&self, lhs: i32, rhs: i32) -> bool {
+        match *self {
+            Less => lhs < rhs,
+            LessEqual => lhs <= rhs,
+            Equal => lhs == rhs,
+            GreaterEqual => lhs >= rhs,
+            Greater => lhs > rhs,
+            NotEqual => lhs != rhs,
+        }
+    }
+}
+
+impl FromStr for Comparison {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s.trim() {
+            "<" => Less,
+            "<=" => LessEqual,
+            "==" => Equal,
+            ">=" => GreaterEqual,
+            ">" => Greater,
+            "!=" => NotEqual,
+            _ => bail!("Could not parse \"{}\" as a comparison", s),
+        })
+    }
+}
+
+#[derive(Clone, Ord, PartialOrd, Eq, PartialEq)]
+struct Instruction {
+    mod_reg: String,
+    mod_val: i32,
+    cond_reg: String,
+    cond: Comparison,
+    cond_val: i32,
+}
+
+impl Instruction {
+    fn new(
+        mod_reg: String,
+        mod_val: i32,
+        cond_reg: String,
+        cond: Comparison,
+        cond_val: i32,
+    ) -> Instruction {
+        Instruction {
+            mod_reg,
+            mod_val,
+            cond_reg,
+            cond,
+            cond_val,
+        }
+    }
+}
+
+impl FromStr for Instruction {
+    type Err = Error;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let mut words = line.split_whitespace();
+        let mod_reg = words.next().unwrap();
+        let mod_val = {
+            let sign: i32 = {
+                match words.next().unwrap() {
+                    "inc" => 1,
+                    "dec" => -1,
+                    direction => bail!("Unknown direction \"{}\"", direction),
+                }
+            };
+            let mod_val_base: i32 = words.next().unwrap().parse()?;
+            sign * mod_val_base
+        };
+        words.next(); // "if"
+        let cond_reg = words.next().unwrap();
+        let cond = Comparison::from_str(words.next().unwrap())?;
+        let cond_val = words.next().unwrap().parse()?;
+
+        Ok(Instruction::new(
+            mod_reg.to_string(),
+            mod_val,
+            cond_reg.to_string(),
+            cond,
+            cond_val,
+        ))
+    }
+}
+
+
+fn read_input() -> Result<Vec<Instruction>, Error> {
+    let mut input = Vec::new();
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line?;
+        let line = line.trim();
+        if !line.is_empty() {
+            input.push(Instruction::from_str(line.as_ref())?)
+        }
+    }
+    Ok(input)
+}
+
+
+
+fn run() -> Result<(), Error> {
+    let instructions = read_input()?;
+
+    let mut registers: HashMap<String, i32> = HashMap::new();
+
+    for instruction in instructions {
+        let cond_reg = *registers.get(&instruction.cond_reg).unwrap_or(&0);
+        if instruction.cond.compare(cond_reg, instruction.cond_val) {
+            let reg_val = registers.entry(instruction.mod_reg).or_insert(0);
+            *reg_val += instruction.mod_val;
+        }
+    }
+
+    let max_register = registers.values().max().unwrap();
+
+    println!("{}", max_register);
+
+    Ok(())
+}
+
+
+fn main() {
+    match run() {
+        Ok(()) => process::exit(0),
+        Err(error) => {
+            for cause in error.causes() {
+                eprintln!("{}", cause)
+            }
+            process::exit(1)
+        }
+    }
+}
---
+++ day8-2/tests/samples.rs
@@ -0,0 +1,27 @@
+extern crate assert_cli;
+
+#[test]
+fn sample1() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "\
+b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10
+             ",
+        )
+        .stdout()
+        .is("10")
+        .unwrap();
+}
+
+
+#[test]
+fn puzzle1() {
+    assert_cli::Assert::main_binary()
+        .stdin(include_str!("../data/puzzle1.in"))
+        .stdout()
+        .is("6582")
+        .unwrap();
+}
