914e577b66623cfdb3e98cd6d5dacb3162f0b32a
Mikael Zayenz Lagerkvist <zayenz@gmail.com>
2017-12-11 09:17:04 +0100

    Solve Day 9 2

---
+++ day9-2/src/main.rs
@@ -0,0 +1,198 @@
+#[macro_use]
+extern crate failure;
+use failure::{Error, Fail};
+
+use std::{io, process};
+use std::collections::HashMap;
+use std::io::BufRead;
+use std::str::FromStr;
+use std::iter::Peekable;
+use std::slice::Iter;
+
+
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+enum Token {
+    GroupStart,
+    GroupEnd,
+    GarbageStart,
+    GarbageEnd,
+    Separator,
+    Escape,
+    Char(char)
+}
+
+use Token::*;
+
+impl Token {
+}
+
+impl From<char> for Token {
+    fn from(c: char) -> Self {
+        match c {
+            '{' => GroupStart,
+            '}' => GroupEnd,
+            '<' => GarbageStart,
+            '>' => GarbageEnd,
+            ',' => Separator,
+            '!' => Escape,
+            _ => Char(c),
+        }
+    }
+}
+
+
+
+fn read_input() -> Result<Vec<Token>, Error> {
+    let mut input = Vec::new();
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line?;
+        let line = line.trim();
+        if !line.is_empty() {
+            for c in line.chars() {
+                input.push(c.into());
+            }
+        }
+    }
+    Ok(input)
+}
+
+#[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq)]
+enum Tree {
+    Group { content: Vec<Tree> },
+    Garbage { content: Vec<Token> },
+}
+
+use Tree::*;
+
+#[derive(Fail, Debug)]
+#[fail(display = "A parse error occurred when reading {:?}: {}", token, message)]
+struct ParseError {
+    token: Option<Token>,
+    message: String,
+}
+
+impl ParseError {
+    fn wrong_token<S: Into<String>>(token: Token, message: S) -> ParseError {
+        ParseError { token: Some(token), message: message.into(), }
+    }
+
+    fn end_of_input<S: Into<String>>(message: S) -> ParseError {
+        ParseError { token: None, message: message.into(), }
+    }
+}
+
+fn parse_garbage(tokens: &mut Peekable<Iter<Token>>) -> Result<Tree, ParseError> {
+    match tokens.peek() {
+        None => Err(ParseError::end_of_input("Expected start of group, but no more tokens.")),
+        Some(&&GarbageStart) => {
+            tokens.next();
+            let mut members = Vec::new();
+            loop {
+                match tokens.peek() {
+                    None => {
+                        return Err(ParseError::end_of_input("Parsing group contents, but no more tokens."));
+                    }
+                    Some(&&GarbageEnd) => {
+                        tokens.next();
+                        break;
+                    },
+                    Some(&&Escape) => {
+                        tokens.next();
+                        tokens.next();
+                    },
+                    Some(_) => {
+                        members.push(*tokens.next().unwrap());
+                    }
+                }
+            }
+            Ok(Garbage { content: members })
+        },
+        Some(&&token) => Err(ParseError::wrong_token(token, "Expected start of garbage."))
+    }
+}
+
+fn parse_group(tokens: &mut Peekable<Iter<Token>>) -> Result<Tree, ParseError> {
+     match tokens.peek() {
+         None => Err(ParseError::end_of_input("Expected start of group, but no more tokens.")),
+         Some(&&GroupStart) => {
+             tokens.next();
+             let mut members = Vec::new();
+
+             loop {
+                 match tokens.peek() {
+                     None => {
+                         return Err(ParseError::end_of_input("Parsing group, but no more tokens."))
+                     },
+                     Some(&&GroupStart) => {
+                         let group = parse_group(tokens)?;
+                         members.push(group);
+                     },
+                     Some(&&Separator) => {
+                         tokens.next();
+                     },
+                     Some(&&GroupEnd) => {
+                         tokens.next();
+                         break;
+                     },
+                     Some(&&GarbageStart) => {
+                         let garbage = parse_garbage(tokens)?;
+                         members.push(garbage);
+                     }
+                     Some(&&token) => {
+                         return Err(ParseError::wrong_token(token, "Wrong token while parsing group contents."))
+                     }
+                 }
+             }
+
+             Ok(Group { content: members })
+         },
+         Some(&&GarbageStart) => {
+             parse_garbage(tokens)
+         }
+         Some(&&token) => Err(ParseError::wrong_token(token, "Expected start of group."))
+     }
+}
+
+fn parse(tokens: &mut Peekable<Iter<Token>>) -> Result<Tree, ParseError> {
+    parse_group(tokens)
+}
+
+
+fn calculate_score(tree: &Tree) -> i32 {
+    match *tree {
+        Group { ref content } => {
+            let sub_tree_score: i32 = content.iter().map(|sub_tree| calculate_score(sub_tree)).sum();
+            sub_tree_score
+        },
+        Garbage { ref content } => {
+            content.len() as i32
+        },
+    }
+}
+
+fn run() -> Result<(), Error> {
+    let tokens = read_input()?;
+
+    let mut token_iterator: Peekable<Iter<Token>> = tokens.iter().peekable();
+    let tree = parse(&mut token_iterator)?;
+//    println!("Tree is {:?}", tree);
+    let score = calculate_score(&tree);
+
+    println!("{:?}", score);
+
+    Ok(())
+}
+
+
+fn main() {
+    match run() {
+        Ok(()) => process::exit(0),
+        Err(error) => {
+            for cause in error.causes() {
+                eprintln!("{}", cause)
+            }
+            process::exit(1)
+        }
+    }
+}
---
+++ day9-2/tests/samples.rs
@@ -0,0 +1,179 @@
+extern crate assert_cli;
+
+#[test]
+fn sample1_1() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "{}",
+        )
+        .stdout()
+        .is("0")
+        .unwrap();
+}
+
+
+#[test]
+fn sample1_2() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "{{{}}}",
+        )
+        .stdout()
+        .is("0")
+        .unwrap();
+}
+
+#[test]
+fn sample1_3() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "{{},{}}",
+        )
+        .stdout()
+        .is("0")
+        .unwrap();
+}
+
+#[test]
+fn sample1_4() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "{{{},{},{{}}}}",
+        )
+        .stdout()
+        .is("0")
+        .unwrap();
+}
+
+#[test]
+fn sample1_5() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "{<a>,<a>,<a>,<a>}",
+        )
+        .stdout()
+        .is("4")
+        .unwrap();
+}
+
+#[test]
+fn sample1_6() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "{{<ab>},{<ab>},{<ab>},{<ab>}}",
+        )
+        .stdout()
+        .is("8")
+        .unwrap();
+}
+
+#[test]
+fn sample1_7() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "{{<!!>},{<!!>},{<!!>},{<!!>}}",
+        )
+        .stdout()
+        .is("0")
+        .unwrap();
+}
+
+#[test]
+fn sample1_8() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "{{<a!>},{<a!>},{<a!>},{<ab>}}",
+        )
+        .stdout()
+        .is("17")
+        .unwrap();
+}
+
+
+#[test]
+fn sampl2_0() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "{<>}",
+        )
+        .stdout()
+        .is("0")
+        .unwrap();
+}
+
+#[test]
+fn sampl2_1() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "{<random characters>}",
+        )
+        .stdout()
+        .is("17")
+        .unwrap();
+}
+
+#[test]
+fn sampl2_2() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "{<<<<>}",
+        )
+        .stdout()
+        .is("3")
+        .unwrap();
+}
+
+#[test]
+fn sampl2_3() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "{<{!>}>}",
+        )
+        .stdout()
+        .is("2")
+        .unwrap();
+}
+
+#[test]
+fn sampl2_4() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "{<!!>}",
+        )
+        .stdout()
+        .is("0")
+        .unwrap();
+}
+
+#[test]
+fn sampl2_5() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "{<!!!>>}",
+        )
+        .stdout()
+        .is("0")
+        .unwrap();
+}
+
+#[test]
+fn sample2_6() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "{<{o\"i!a,<{i<a>}",
+        )
+        .stdout()
+        .is("10")
+        .unwrap();
+}
+
+
+
+#[test]
+fn puzzle1() {
+    assert_cli::Assert::main_binary()
+        .stdin(include_str!("../data/puzzle1.in"))
+        .stdout()
+        .is("10045")
+        .unwrap();
+}
