b650921067a9fb36f1752506a58d9f7894fa368a
Mikael Zayenz Lagerkvist <zayenz@gmail.com>
2017-12-12 06:26:45 +0100

    Solve Day 12 1

---
+++ day12-1/src/main.rs
@@ -0,0 +1,96 @@
+extern crate failure;
+use failure::Error;
+
+use std::{io, process};
+use std::io::BufRead;
+use std::cmp::max;
+
+
+
+fn read_input() -> Result<(i32, Vec<Vec<i32>>), Error> {
+    let mut largest = 0;
+    let mut input: Vec<Vec<i32>> = Vec::new();
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line?;
+        let mut words = line.trim().split_whitespace();
+        if let Some(first_word) = words.next() {
+            let mut group = Vec::new();
+            group.push(first_word.parse()?);
+            words.next(); // <->
+            for word in words {
+                group.push(word.trim_right_matches(',').parse()?)
+            }
+            largest = max(largest, *group.iter().max().unwrap());
+            input.push(group);
+        }
+    }
+    Ok((largest, input))
+}
+
+#[derive(Debug)]
+struct UnionFind {
+    nodes: Vec<i32>,
+}
+
+impl UnionFind {
+    fn new(size: i32) -> UnionFind {
+        let mut nodes = Vec::with_capacity(size as usize);
+        for _ in 1..size + 1 {
+            nodes.push(-1);
+        }
+        UnionFind { nodes }
+    }
+
+    fn find(&self, node: i32) -> i32 {
+        assert!(0 <= node && (node as usize) < self.nodes.len());
+        let root = self.nodes[node as usize];
+        if root >= 0 { self.find(root) } else { node }
+    }
+
+    fn join(&mut self, node1: i32, node2: i32) {
+        let root1 = self.find(node1);
+        let root2 = self.find(node2);
+        if root1 != root2 {
+            self.nodes[root1 as usize] += self.nodes[root2 as usize];
+            self.nodes[root2 as usize] = root1;
+        }
+    }
+
+    fn size(&self, node: i32) -> i32 {
+        let root = self.find(node);
+        self.nodes[root as usize].abs()
+    }
+}
+
+
+fn run() -> Result<(), Error> {
+    let (largest, input_groups) = read_input()?;
+
+    let mut groups = UnionFind::new(largest + 1);
+
+    for input_group in &input_groups {
+        for a in input_group {
+            for b in input_group {
+                groups.join(*a, *b);
+            }
+        }
+    }
+
+    println!("{}", groups.size(0));
+
+    Ok(())
+}
+
+
+fn main() {
+    match run() {
+        Ok(()) => process::exit(0),
+        Err(error) => {
+            for cause in error.causes() {
+                eprintln!("{}", cause)
+            }
+            process::exit(1)
+        }
+    }
+}
---
+++ day12-1/tests/samples.rs
@@ -0,0 +1,29 @@
+extern crate assert_cli;
+
+#[test]
+fn sample1() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "\
+            0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5",
+        )
+        .stdout()
+        .is("6")
+        .unwrap();
+}
+
+
+#[test]
+fn puzzle1() {
+    assert_cli::Assert::main_binary()
+        .stdin(include_str!("../data/puzzle1.in"))
+        .stdout()
+        .is("175")
+        .unwrap();
+}
