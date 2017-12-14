13e190f799b104537c7157a0de0a0ae6265861c1
Mikael Zayenz Lagerkvist <zayenz@gmail.com>
2017-12-14 16:46:49 +0100

    Use library UnionFind for Day 14 part 2

--- day14-2/src/main.rs
+++ day14-2/src/main.rs
@@ -9,6 +9,9 @@ use failure::Error;
 use std::{io, process};
 use std::io::BufRead;
 
+extern crate aoc2017;
+use aoc2017::UnionFind;
+
 fn read_input() -> Result<String, Error> {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
@@ -20,52 +23,6 @@ fn read_input() -> Result<String, Error> {
     }
     bail!("No input")
 }
-
-#[derive(Debug)]
-struct UnionFind {
-    nodes: Vec<i32>,
-}
-
-impl UnionFind {
-    fn new(size: i32) -> UnionFind {
-        let mut nodes = Vec::with_capacity(size as usize);
-        for _ in 1..size + 1 {
-            nodes.push(-1);
-        }
-        UnionFind { nodes }
-    }
-
-    fn find(&self, node: i32) -> i32 {
-        assert!(0 <= node && (node as usize) < self.nodes.len());
-        let root = self.nodes[node as usize];
-        if root >= 0 { self.find(root) } else { node }
-    }
-
-    fn join(&mut self, node1: i32, node2: i32) {
-        let root1 = self.find(node1);
-        let root2 = self.find(node2);
-        if root1 != root2 {
-            self.nodes[root1 as usize] += self.nodes[root2 as usize];
-            self.nodes[root2 as usize] = root1;
-        }
-    }
-
-    fn size(&self, node: i32) -> i32 {
-        let root = self.find(node);
-        self.nodes[root as usize].abs()
-    }
-
-    fn group_count(&self) -> i32 {
-        let mut result = 0;
-        for &node in &self.nodes {
-            if node < 0 {
-                result += 1;
-            }
-        }
-        result
-    }
-}
-
 
 const DATA_LENGTH: usize = 256;
 
