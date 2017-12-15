73762096fef7d80c6cb64efd89f1a6cb89e912cd
Mikael Zayenz Lagerkvist <zayenz@gmail.com>
2017-12-15 06:14:56 +0100

    Solve Day 15 1

---
+++ day15-1/tests/samples.rs
@@ -0,0 +1,25 @@
+extern crate assert_cli;
+
+#[test]
+fn sample1() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "\
+Generator A starts with 65
+Generator B starts with 8921
+",
+        )
+        .stdout()
+        .is("588")
+        .unwrap();
+}
+
+
+#[test]
+fn puzzle1() {
+    assert_cli::Assert::main_binary()
+        .stdin(include_str!("../data/puzzle1.in"))
+        .stdout()
+        .is("600")
+        .unwrap();
+}
