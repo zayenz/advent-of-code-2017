c5340d528f9cedf59edaf873d3884c3ed65b07c8
Mikael Zayenz Lagerkvist <zayenz@gmail.com>
2017-12-15 10:51:45 +0100

    Change Day 15 2 to use functional style

--- day15-2/src/main.rs
+++ day15-2/src/main.rs
@@ -13,61 +13,69 @@ use std::io::BufRead;
 extern crate aoc2017;
 use aoc2017::UnionFind;
 
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
+struct Generator {
+    value: u64,
+    factor: u64,
+    multiple: u64,
+}
+
+impl Generator {
+    fn new(value: u64, factor: u64, multiple: u64) -> Generator {
+        Generator {
+            value,
+            factor,
+            multiple,
+        }
+    }
+}
+
+impl Iterator for Generator {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            self.value = (self.value * self.factor) % 2_147_483_647;
+            if self.value % self.multiple == 0 {
+                return Some(self.value);
+            }
+        }
+    }
+}
+
 fn get_number_from_line(line: &str) -> Result<u64, Error> {
     let number = line.split_whitespace().nth(4).unwrap().parse()?;
     Ok(number)
 }
 
-fn read_input() -> Result<(u64, u64), Error> {
+fn read_input() -> Result<(Generator, Generator), Error> {
     let stdin = io::stdin();
     let mut lines = stdin.lock().lines();
-    let a = get_number_from_line(&lines.next().unwrap()?)?;
-    let b = get_number_from_line(&lines.next().unwrap()?)?;
-    Ok((a, b))
-}
+    let a_start = get_number_from_line(&lines.next().unwrap()?)?;
+    let b_start = get_number_from_line(&lines.next().unwrap()?)?;
 
-fn next(v: u64, factor: u64, multiple: u64) -> u64 {
-    const A_FACTOR: u64 = 16_807;
-    let mut value = v;
-    loop {
-        value = (value * factor) % 2_147_483_647;
-        if value % multiple == 0 {
-            return value;
-        }
-    }
-
-}
-
-fn next_a(v: u64) -> u64 {
     const A_FACTOR: u64 = 16_807;
     const A_MULTIPLE: u64 = 4;
-    next(v, A_FACTOR, A_MULTIPLE)
-}
-
-fn next_b(v: u64) -> u64 {
     const B_FACTOR: u64 = 48_271;
     const B_MULTIPLE: u64 = 8;
-    next(v, B_FACTOR, B_MULTIPLE)
-}
 
+    let a = Generator::new(a_start, A_FACTOR, A_MULTIPLE);
+    let b = Generator::new(b_start, B_FACTOR, B_MULTIPLE);
+
+    Ok((a, b))
+}
 
 fn matches(a: u64, b: u64) -> bool {
     (a & 0xFFFF) == (b & 0xFFFF)
 }
 
-
 fn run() -> Result<(), Error> {
-    let (mut a, mut b) = read_input()?;
+    let (a, b) = read_input()?;
 
-    let mut matches_count = 0;
-
-    for _ in 0..5_000_000 {
-        a = next_a(a);
-        b = next_b(b);
-        if matches(a, b) {
-            matches_count += 1;
-        }
-    }
+    let matches_count = a.zip(b)
+        .take(5_000_000)
+        .filter(|&(a, b)| matches(a, b))
+        .count();
 
     println!("{}", matches_count);
 
