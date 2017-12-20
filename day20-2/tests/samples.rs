4bb98d1bb0533eabee118c6ea0daadea4c53cad0
Mikael Zayenz Lagerkvist <zayenz@gmail.com>
2017-12-20 14:23:59 +0100

    Solve Day 20 2

---
+++ day20-2/tests/samples.rs
@@ -0,0 +1,26 @@
+extern crate assert_cli;
+
+#[test]
+fn sample1() {
+    assert_cli::Assert::main_binary()
+        .stdin(
+            "\
+p=<-6,0,0>, v=< 3,0,0>, a=< 0,0,0>
+p=<-4,0,0>, v=< 2,0,0>, a=< 0,0,0>
+p=<-2,0,0>, v=< 1,0,0>, a=< 0,0,0>
+p=< 3,0,0>, v=<-1,0,0>, a=< 0,0,0>",
+        )
+        .stdout()
+        .is("1")
+        .unwrap();
+}
+
+
+#[test]
+fn puzzle1() {
+    assert_cli::Assert::main_binary()
+        .stdin(include_str!("../data/puzzle1.in"))
+        .stdout()
+        .is("404")
+        .unwrap();
+}
